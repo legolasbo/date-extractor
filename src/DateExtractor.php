49ee063e4b40ce164439292cbcea254e6cc9a45b
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2016-01-24 20:58:14 +0100

    Added handling for numeric dates

--- src/DateExtractor.php
+++ src/DateExtractor.php
@@ -2,6 +2,8 @@
 
 namespace legolasbo\DateExtractor;
 
+use DateTime;
+
 class DateExtractor {
 
   private $textToSearch;
@@ -13,7 +15,71 @@ class DateExtractor {
     $this->textToSearch = $textToSearch;
   }
 
+  /**
+   * @return bool
+   */
   public function containsDate() {
-    return FALSE;
+    return !empty($this->getDateAsArray());
+  }
+
+  /**
+   * @return \DateTime
+   */
+  public function getDateTimeObject() {
+    $result = new DateTime();
+
+    if ($date = $this->getDateAsArray()) {
+      $result->setDate($date['year'], $date['month'], $date['day']);
+      $result->setTime(0,0);
+    }
+
+    return $result;
+  }
+
+  /**
+   * @return array
+   */
+  public function getDateAsArray() {
+    $result = [];
+
+    if (preg_match('(\b(\d{1,2}|\d{4}|[a-zA-Z]+)[-| |\/](\d{1,2}|[a-zA-Z]+)[-| |\/](\d{4}|\d{1,2})\b)', $this->textToSearch, $matches)) {
+      $result['year'] = $matches[3];
+      $result['month'] = $matches[2];
+      $result['day'] = $matches[1];
+    }
+
+    return $this->ensureValidResult($result);
+  }
+
+  /**
+   * @param array $result
+   * @return array
+   */
+  private function ensureValidResult(array $result) {
+    if (empty($result)) {
+      return $result;
+    }
+
+    if (strlen($result['day']) > 2 && strlen($result['year']) <= 2) {
+      $result = $this->swapValuesOfKeys($result, 'day', 'year');
+    }
+
+    if ($result['month'] > 12) {
+      $result = $this->swapValuesOfKeys($result, 'day', 'month');
+    }
+
+    return $result;
+  }
+
+  /**
+   * @param array $result
+   * @return array
+   */
+  private function swapValuesOfKeys(array $result, $key1, $key2) {
+    $temp = $result[$key1];
+    $result[$key1] = $result[$key2];
+    $result[$key2] = $temp;
+
+    return $result;
   }
 }
--- tests/DateExtractorTest.php
+++ tests/DateExtractorTest.php
@@ -2,6 +2,7 @@
 
 namespace legolasbo\DateExtractor\tests;
 
+use DateTime;
 use legolasbo\DateExtractor\DateExtractor;
 use PHPUnit_Framework_TestCase;
 
@@ -10,8 +11,49 @@ class DateExtractorTest extends PHPUnit_Framework_TestCase {
   /**
    * @test
    */
-  public function emptyStringReturnsNoExtractedDate() {
-    $extractor = new DateExtractor('');
+  public function singleDateExpectedToBeNotFound() {
+    $this->assertNoDateFound('');
+    $this->assertNoDateFound('llalala31-01-2015');
+    $this->assertNoDateFound('llalala31-01-2015sgsdgs');
+    $this->assertNoDateFound('31-01-2015sgsdgs');
+  }
+
+  /**
+   * @test
+   */
+  public function singleDateExtractedCorrectly() {
+    $this->assertResultForSingleDatePresent('31-01-2015', '31-01-2015');
+    $this->assertResultForSingleDatePresent('01-31-2015', '31-01-2015');
+    $this->assertResultForSingleDatePresent('31-1-2015', '31-01-2015');
+    $this->assertResultForSingleDatePresent('1-31-2015', '31-01-2015');
+    $this->assertResultForSingleDatePresent('2015-01-31', '31-01-2015');
+    $this->assertResultForSingleDatePresent('2015-31-01', '31-01-2015');
+    $this->assertResultForSingleDatePresent('hello 2015-31-01', '31-01-2015');
+    $this->assertResultForSingleDatePresent('hello 2015-31-01 there', '31-01-2015');
+    $this->assertResultForSingleDatePresent('31/01/2015', '31-01-2015');
+    $this->assertResultForSingleDatePresent('31 01 2015', '31-01-2015');
+  }
+
+  public function assertNoDateFound($text) {
+    $extractor = new DateExtractor($text);
     $this->assertFalse($extractor->containsDate());
   }
+
+  /**
+   * @param $text
+   * @param $expected_date
+   */
+  public function assertResultForSingleDatePresent($text, $expected_date) {
+    $extractor = new DateExtractor($text);
+
+    $expected = [
+      'year' => 2015,
+      'month' => 1,
+      'day' => 31,
+    ];
+    $this->assertTrue($extractor->containsDate());
+    $this->assertEquals($expected, $extractor->getDateAsArray());
+    $this->assertEquals(new DateTime($expected_date), $extractor->getDateTimeObject());
+  }
+
 }
